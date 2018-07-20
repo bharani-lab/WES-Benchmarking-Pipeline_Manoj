c029633b9d3451e31ad34113413112397d511993
Dr. Bharanidharan devarajan <35055033+bharani-lab@users.noreply.github.com>
2018-07-20 09:20:29 +0530

    Update Novoalign_Freebayes.sh

--- Script/Novoalign_Freebayes.sh
+++ Script/Novoalign_Freebayes.sh
@@ -13,16 +13,8 @@ java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar SortSam VALIDATION_STRINGE
 java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar MarkDuplicates VALIDATION_STRINGENCY=SILENT I=output/"$r1"/"$r1"_Sort_novoalign.bam O=output/"$r1"/"$r1"_PCR_novoalign.bam REMOVE_DUPLICATES=true M=output/"$r1"/"$r1"_pcr_novoalign.metrics
 #ID_Addition
 java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar AddOrReplaceReadGroups VALIDATION_STRINGENCY=SILENT I=output/"$r1"/"$r1"_PCR_novoalign.bam O=output/"$r1"/"$r1"_RG_novoalign.bam SO=coordinate RGID=SRR"$r1" RGLB=SRR"$r1" RGPL=illumina RGPU=SRR"$r1" RGSM=SRR"$r1" CREATE_INDEX=true
-#Realignmnet_Metrix
-java -Xmx10g -jar Tools/GenomeAnalysisTK.jar -T RealignerTargetCreator -R "$Re1" -I output/"$r1"/"$r1"_RG_novoalign.bam -o output/"$r1"/"$r1"_Realignement_novoalign.list --filter_mismatching_base_and_quals
-#Realignment
-java -Xmx10g -jar Tools/GenomeAnalysisTK.jar -T IndelRealigner -R "$Re1" -targetIntervals output/"$r1"/"$r1"_Realignement.list -I output/"$r1"/"$r1"_RG_novoalign.bam -o output/"$r1"/"$r1"_Realignment_novoalign.bam --filter_mismatching_base_and_quals
-#baseQuality_Check
-java -jar Tools/GenomeAnalysisTK.jar -T BaseRecalibrator -R "$Re1" -I output/"$r1"/"$r1"_Realignment_novoalign.bam -o output/"$r1"/"$r1"_Realignment_novoalign_data.table
-#quality
-java -jar Tools/GenomeAnalysisTK.jar -T PrintReads -R "$Re1" -I output/"$r1"/"$r1"_Realignment_novoalign.bam -BQSR output/"$r1"/"$r1"_Realignment_novoalign_data.table -o output/"$r1"/"$r1"_novoalign_recal.bam
 #Variant_Calling-Free_Bayes
-freebayes -f -f "$Re1" -v output/"$r1"/novoalign_Freebayes_"$r1".vcf output/"$r1"/"$r1"_novoalign_recal.bam
+freebayes -f -f "$Re1" -v output/"$r1"/novoalign_Freebayes_"$r1".vcf output/"$r1"/"$r1"_RG_novoalign.bam
 #variant_Sepration_Indel_SNV
 vcftools --vcf output/"$r1"/novoalign_Freebayes_"$r1".vcf --remove-indels --recode --recode-INFO-all --out output/"$r1"/novoalign_FreeBayes_SNP_"$r1".vcf
 vcftools --vcf output/"$r1"/novoalign_Freebayes_"$r1".vcf --keep-only-indels  --recode --recode-INFO-all --out output/"$r1"/novoalign_FreeBayes_Indels_"$r1".vcf
