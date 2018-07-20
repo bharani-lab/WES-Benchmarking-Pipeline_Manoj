41cb5beceef179386203610baa9ed746cb69e55c
Dr. Bharanidharan devarajan <35055033+bharani-lab@users.noreply.github.com>
2018-07-20 09:20:02 +0530

    Update Novoalign_DeepVariant.sh

--- Script/Novoalign_DeepVariant.sh
+++ Script/Novoalign_DeepVariant.sh
@@ -13,14 +13,6 @@ java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar SortSam VALIDATION_STRINGE
 java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar MarkDuplicates VALIDATION_STRINGENCY=SILENT I=output/"$r1"/"$r1"_Sort_novoalign.bam O=output/"$r1"/"$r1"_PCR_novoalign.bam REMOVE_DUPLICATES=true M=output/"$r1"/"$r1"_pcr_novoalign.metrics
 #ID_Addition
 java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar AddOrReplaceReadGroups VALIDATION_STRINGENCY=SILENT I=output/"$r1"/"$r1"_PCR_novoalign.bam O=output/"$r1"/"$r1"_RG_novoalign.bam SO=coordinate RGID=SRR"$r1" RGLB=SRR"$r1" RGPL=illumina RGPU=SRR"$r1" RGSM=SRR"$r1" CREATE_INDEX=true
-#Realignmnet_Metrix
-java -Xmx10g -jar Tools/GenomeAnalysisTK.jar -T RealignerTargetCreator -R "$Re1" -I output/"$r1"/"$r1"_RG_novoalign.bam -o output/"$r1"/"$r1"_Realignement_novoalign.list --filter_mismatching_base_and_quals
-#Realignment
-java -Xmx10g -jar Tools/GenomeAnalysisTK.jar -T IndelRealigner -R "$Re1" -targetIntervals output/"$r1"/"$r1"_Realignement.list -I output/"$r1"/"$r1"_RG_novoalign.bam -o output/"$r1"/"$r1"_Realignment_novoalign.bam --filter_mismatching_base_and_quals
-#baseQuality_Check
-java -jar Tools/GenomeAnalysisTK.jar -T BaseRecalibrator -R "$Re1" -I output/"$r1"/"$r1"_Realignment_novoalign.bam -o output/"$r1"/"$r1"_Realignment_novoalign_data.table
-#quality
-java -jar Tools/GenomeAnalysisTK.jar -T PrintReads -R "$Re1" -I output/"$r1"/"$r1"_Realignment_novoalign.bam -BQSR output/"$r1"/"$r1"_Realignment_novoalign_data.table -o output/"$r1"/"$r1"_novoalign_recal.bam
 #Variant_Calling-Deep_Variant
 MODEL=DeepVariant-inception_v3-0.4.0+cl-174375304.data-wgs_standard/model.ckpt
 OUTPUT_DIR="$r1"_novoalign
@@ -28,7 +20,7 @@ mkdir -p "${OUTPUT_DIR}"
 LOGDIR=./novoalign_logs
 N_SHARDS=10
 mkdir -p "${LOGDIR}"
-time seq 0 $((N_SHARDS-1)) | parallel --eta --halt 2 --joblog "${LOGDIR}/log" --res "${LOGDIR}" python bin/make_examples.zip  --mode calling --ref "$Re1" --reads output/"$r1"/"$r1"_novoalign_recal.bam --examples "${OUTPUT_DIR}/examples.tfrecord@${N_SHARDS}.gz" --task {}
+time seq 0 $((N_SHARDS-1)) | parallel --eta --halt 2 --joblog "${LOGDIR}/log" --res "${LOGDIR}" python bin/make_examples.zip  --mode calling --ref "$Re1" --reads output/"$r1"/"$r1"_RG_novoalign.bam --examples "${OUTPUT_DIR}/examples.tfrecord@${N_SHARDS}.gz" --task {}
 python bin/call_variants.zip --outfile output/"$r1"/novoalign_Deep_"$r1".gz --examples "${OUTPUT_DIR}/examples.tfrecord@${N_SHARDS}.gz" --checkpoint "${MODEL}"
 python bin/postprocess_variants.zip --ref "$Re1" --infile output/"$r1"/novoalign_Deep_"$r1".gz --outfile output/"$r1"/novoalign_Deep_"$r1".vcf
 #variant_Sepration_Indel_SNV
