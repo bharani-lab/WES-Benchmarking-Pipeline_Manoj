8fe33528ecf22877a6afe3e317c48dfae18a2f91
Dr. Bharanidharan devarajan <35055033+bharani-lab@users.noreply.github.com>
2018-07-20 09:23:07 +0530

    Update Soap_Samtools.sh

--- Script/Soap_Samtools.sh
+++ Script/Soap_Samtools.sh
@@ -15,16 +15,8 @@ java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar SortSam VALIDATION_STRINGE
 java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar MarkDuplicates VALIDATION_STRINGENCY=SILENT I=output/"$r1"/"$r1"_Sort_soap.bam O=output/"$r1"/"$r1"_PCR_soap.bam REMOVE_DUPLICATES=true M=output/"$r1"/"$r1"_pcr_soap.metrics
 #ID_Addition
 java -Xmx10g -jar Tools/picard-tools-1.141/picard.jar AddOrReplaceReadGroups VALIDATION_STRINGENCY=SILENT I=output/"$r1"/"$r1"_PCR_soap.bam O=output/"$r1"/"$r1"_RG_soap.bam SO=coordinate RGID=SRR"$r1" RGLB=SRR"$r1" RGPL=illumina RGPU=SRR"$r1" RGSM=SRR"$r1" CREATE_INDEX=true
-#Realignmnet_Metrix
-java -Xmx10g -jar Tools/GenomeAnalysisTK.jar -T RealignerTargetCreator -R "$Re1" -I output/"$r1"/"$r1"_RG_soap.bam -o output/"$r1"/"$r1"_Realignement_soap.list --filter_mismatching_base_and_quals
-#Realignment
-java -Xmx10g -jar Tools/GenomeAnalysisTK.jar -T IndelRealigner -R "$Re1" -targetIntervals output/"$r1"/"$r1"_Realignement.list -I output/"$r1"/"$r1"_RG_soap.bam -o output/"$r1"/"$r1"_Realignment_soap.bam --filter_mismatching_base_and_quals
-#baseQuality_Check
-java -jar Tools/GenomeAnalysisTK.jar -T BaseRecalibrator -R "$Re1" -I output/"$r1"/"$r1"_Realignment_soap.bam -o output/"$r1"/"$r1"_Realignment_soap_data.table
-#quality
-java -jar Tools/GenomeAnalysisTK.jar -T PrintReads -R "$Re1" -I output/"$r1"/"$r1"_Realignment_soap.bam -BQSR output/"$r1"/"$r1"_Realignment_soap_data.table -o output/"$r1"/"$r1"_soap_recal.bam
 #Variant_Calling-Samtools
-samtools mpileup -go output/"$r1"/soap_samtools_"$r1".bcf -f "$Re1" output/"$r1"/"$r1"_soap_recal.bam
+samtools mpileup -go output/"$r1"/soap_samtools_"$r1".bcf -f "$Re1" output/"$r1"/"$r1"_RG_soap.bam
 #bcf_to_VCF_converstion
 bcftools call -vmO z -o output/"$r1"/soap_samtools_"$r1".vcf output/"$r1"/soap_samtools_"$r1".bcf
 #variant_Sepration_Indel_SNV
